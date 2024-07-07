bacfa9c355f655bac1b8b2e9386cf1dfe16ed3cc
Daval <davalcato@yahoo.com>
2024-07-06 18:01:53 -0700

    Initial Commit

---
+++ EyeAeon/EyeAeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeAeonApp.swift
+//  EyeAeon
+//
+//  Created by Ethan Hunt on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EyeAeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
